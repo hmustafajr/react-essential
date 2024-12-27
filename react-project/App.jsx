84ee95481e9725b1c13399ba3192008aec2473a4
Hassan (mufasa) <hmustafajr@gmail.com>
2024-12-27 15:18:27 -0800

    Add files via upload

---
+++ react-project/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>Cafe Mustafa</h1>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <main>
+      <h2> We serve the most delicious coffee</h2>
+      </main>
+    </div>
+  )
+}
+
+export default App
